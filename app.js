20f3d32804f94cc49d402f4b20913ff52641371f
Raul Quintero <raulquintero13@gmail.com>
2022-04-27 14:52:19 -0700

    adding Fizzbuzz Class & unit test

--- app.js
+++ app.js
@@ -1,63 +1,21 @@
-const fs = require("fs");
+const ExplorerService = require('./lib/services/ExplorerService.js')
+const FizzbuzzService = require('./lib/services/FizzbuzzService.js')
+const Reader = require('./lib/utils/Reader.js')
 
-// Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+const explorers = Reader.readJsonFile('explorers.json')
 
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+const explorersByMission = ExplorerService.filterByMission(explorers, "node");
+console.log('explorers by mission ---->', explorersByMission)
 
-// Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+const amountOfExplorersByMission = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+console.log('amount of explorers by mission ---->', amountOfExplorersByMission)
 
-// DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
+const explorersUsernamesByMission = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+console.log('explorers usernames by mission ---->', explorersUsernamesByMission)
 
-// Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
-// Score: 3, Trick: FIZZ.
-// Score: 4, Trick: 4.
-// Score: 5, Trick: 5.
+// Part7: Get a new list of explorers in Node, if the score number is divisible by 3 
+// AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be
+// the same score value
+const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorers, "node");
+console.log('explorers validated ---->>>', explorersValidated)
 
-const assignFizzTrick = function(explorer){
-    if(explorer.score%3 === 0){
-        explorer.trick = "FIZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
-
-// Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
-//
-const assignBuzzTrick = function(explorer){
-    if(explorer.score%5 === 0){
-        explorer.trick = "BUZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzzTrick(explorer));
-
-//Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be the same score value
-
-const assignFizzBuzzTrick = function(explorer){
-    if(explorer.score%5 === 0 && explorer.score%3 === 0){
-        explorer.trick = "FIZZBUZZ";
-        return explorer;
-    }else{
-        explorer.trick = explorer.score;
-        return explorer;
-    }
-};
-
-const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
-
-// Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
---
+++ test/lib/services/FizzbuzzService.test.js
@@ -0,0 +1,37 @@
+const FizzbuzzService = require('../../../lib/services/FizzbuzzService.js')
+const Reader = require('../../../lib/utils/Reader.js')
+
+// const explorers = Reader.readJsonFile('explorers.json')
+
+
+describe('test for ExplorerService Class', ()=> {
+    test('test for different of 3 or 5', ()=> {
+        const explorer1 = [{name: "Explorer1", score: 1}]
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorersValidated).toEqual(
+            expect.arrayContaining([{name: "Explorer1", score: 1, trick: 1}]),
+        );
+    })
+
+    test('test when is equal to %3', ()=> {
+        const explorer1 = [{name: "Explorer3", score: 3}]
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorersValidated).toEqual(
+            expect.arrayContaining([{name: "Explorer3", score: 3, trick: "FIZZ"}]),
+        );
+    })
+    test('test when is equal to %5', ()=> {
+        const explorer1 = [{name: "Explorer5", score: 5}]
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorersValidated).toEqual(
+            expect.arrayContaining([{name: "Explorer5", score: 5, trick: "BUZZ"}]),
+        );
+    })
+    test('test when is equal to %3 or %5', ()=> {
+        const explorer1 = [{name: "Explorer15", score: 15}]
+        const explorersValidated = FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorersValidated).toEqual(
+            expect.arrayContaining([{name: "Explorer15", score: 15, trick: "FIZZBUZZ"}]),
+        );
+    })
+})
