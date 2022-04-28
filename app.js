cce2a46591e790416bfcff5e447f8f02ccc3a047
Raul Quintero <raulquintero13@gmail.com>
2022-04-27 17:56:51 -0700

    Adding ESLint & some files fixed & FizzBuzzService Fixed

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-const express = require('express')
-const ExplorerService = require('./lib/services/ExplorerService.js')
-const Reader = require('./lib/utils/Reader.js')
+const express = require("express");
+const ExplorerService = require("./lib/services/ExplorerService.js");
+const Reader = require("./lib/utils/Reader.js");
 
-const explorers = Reader.readJsonFile('explorers.json')
+const explorers = Reader.readJsonFile("explorers.json");
 
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send("hola")
-})
+app.get("/", (req,res) => {
+    res.send("hola");
+});
 
-app.get('/v1/explorers/:mission', (req, res) => {
+app.get("/v1/explorers/:mission", (req, res) => {
     res.send({explorers: ExplorerService.filterByMission(explorers, req.params.mission)});
-})
-app.get('/v1/explorers/amount/:mission', (req, res) => {
-    const amount = ExplorerService.getAmountOfExplorersByMission(explorers, req.params.mission)
+});
+app.get("/v1/explorers/amount/:mission", (req, res) => {
+    const amount = ExplorerService.getAmountOfExplorersByMission(explorers, req.params.mission);
     res.status(200).send({amount: amount});
 
-})
-app.get('/v1/explorers/usernames/:mission', (req, res) => {
+});
+app.get("/v1/explorers/usernames/:mission", (req, res) => {
     res.send({usernames: ExplorerService.getExplorersUsernamesByMission(explorers, req.params.mission)});
-})
+});
 
 //mostrar en navegador errores 
 app.use((err,req,res,next)=>{
 
-    console.log(err)
+    console.log(err);
 
     res.status(500).json({
         error : {
             message : err.message
-       }
+        }
     });
     
-})
+});
 
 app.listen(port, ()=>{
-    console.log("server listo")
-})
+    console.log("server listo");
+});
 
 
 
