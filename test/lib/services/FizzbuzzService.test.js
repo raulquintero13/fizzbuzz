65bdcc38cb64e7dc3e7442e4e8d9c6f390954706
Raul Quintero <raulquintero13@gmail.com>
2022-04-27 22:59:01 -0700

    applying ESlint

--- test/lib/services/FizzbuzzService.test.js
+++ test/lib/services/FizzbuzzService.test.js
@@ -35,10 +35,10 @@ describe("test for FizzbuzzService Class", ()=> {
         );
     });
 
-    test('test for applyValidationInNumber ', ()=>{
-        expect(FizzbuzzService.applyValidationInNumber(1)).toBe(1)
-        expect(FizzbuzzService.applyValidationInNumber(3)).toBe("FIZZ")
-        expect(FizzbuzzService.applyValidationInNumber(5)).toBe("BUZZ")
-        expect(FizzbuzzService.applyValidationInNumber(15)).toBe("FIZZBUZZ")
-    })
+    test("test for applyValidationInNumber ", ()=>{
+        expect(FizzbuzzService.applyValidationInNumber(1)).toBe(1);
+        expect(FizzbuzzService.applyValidationInNumber(3)).toBe("FIZZ");
+        expect(FizzbuzzService.applyValidationInNumber(5)).toBe("BUZZ");
+        expect(FizzbuzzService.applyValidationInNumber(15)).toBe("FIZZBUZZ");
+    });
 });
